d0115e950741a7f1369f53bc91ff06bad8fbf905
Rick Booth <r.booth@9xb.com>
2021-04-14 20:48:55 +0100

    feat: update favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import '@dracula/dracula-ui/styles/dracula-ui.css'
-
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function App(props: AppProps) {
   const { Component, pageProps } = props
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>rickbooth.me</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
