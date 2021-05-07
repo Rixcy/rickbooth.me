5c9727ea3fd57ba856ba2857557adaaebb2c55fd
Rick Booth <r.booth@9xb.com>
2021-05-07 22:28:27 +0100

    feat: improve seo tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,12 +12,32 @@ function App(props: AppProps) {
         <title>Rick Booth - Software Developer</title>
         <meta
           name="description"
-          content="Software Developer from Skipton specialising in Web Development using modern technologies such as React and TypeScript."
+          content="Rick Booth is a Software
+            Developer from Skipton, England. He specialises in front end web
+            development using modern technologies such as React and TypeScript"
         />
+
+        {/*  Open graph data  */}
         <meta property="og:title" content="Rick Booth - Software Developer" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://github.com/Rixcy.png" />
         <meta property="og:locale" content="en_GB" />
+        <meta
+          property="og:description"
+          content="Rick Booth is a Software
+            Developer from Skipton, England. He specialises in front end web
+            development using modern technologies such as React and TypeScript"
+        />
+
+        {/*  Twitter  */}
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="Rick Booth - Software
+          Developer"
+        />
+        <meta name="twitter:url" content="https://rickbooth.me" />
+        <meta name="twitter:image" content="https://github.com/Rixcy.png" />
       </Head>
       <Layout>
         <Component {...pageProps} />
